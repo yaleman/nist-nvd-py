3d59c7e62dae3339246aacc51700d1f0bd711213
James Hodgkinson <jhodgkinson@splunk.com>
2025-02-05 16:57:46 +1000

    feat: adding pub start/end date to query

---
+++ nist_nvd/validator.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from typing import Optional
+
+
+def validate_iso_format(date_str: str, start: bool) -> Optional[datetime]:
+    if len(date_str.split("T")) == 0:
+        if start:
+            date_str = date_str + "T00:00:00Z"
+        else:
+            date_str = date_str + "T23:59:59Z"
+    try:
+        return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+    except ValueError:
+        return None
---
+++ tests/test_validator.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from nist_nvd.validator import validate_iso_format
+
+
+def test_validate_iso_format() -> None:
+    assert validate_iso_format("2021-01-01", start=True) is None
+    assert validate_iso_format("2021-01-01", start=False) is None
+    assert validate_iso_format("2021-01-01T00:00:00Z", start=True) == datetime(
+        2021, 1, 1, 0, 0, 0
+    )
+    assert validate_iso_format("2021-01-01T00:00:00Z", start=False) == datetime(
+        2021, 1, 1, 0, 0, 0
+    )
+    assert validate_iso_format("2021-01-01T23:59:59Z", start=True) == datetime(
+        2021, 1, 1, 23, 59, 59
+    )
+    assert validate_iso_format("2021-01-01T23:59:59Z", start=False) == datetime(
+        2021, 1, 1, 23, 59, 59
+    )
+    assert validate_iso_format("2021-01-01T00:00:00", start=True) is None
+    assert validate_iso_format("2021-01-01T00:00:00", start=False) is None
+    assert validate_iso_format("2021-01-01T23:59:59", start=True) is None
+    assert validate_iso_format("2021-01-01T23:59:59", start=False) is None
+    assert validate_iso_format("2021-01-01T00:00:00ZT", start=True) is None
+    assert validate_iso_format("2021-01-01T00:00:00ZT", start=False) is None
+    assert validate_iso_format("2021-01-01T23:59:59ZT", start=True) is None
+    assert validate_iso_format("2021-01-01T23:59:59ZT", start=False) is None
+    assert validate_iso_format("2021-01-01T00:00:00ZT00:00:00Z", start=True) is None
+    assert validate_iso_format("2021-01-01T00:00:00ZT00:00:00Z", start=False) is None
